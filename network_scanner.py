56f0cc5e2157601f9f73646e8ba5be946bd37a83
root <hackenstance@proton.me>
2026-03-17 12:30:37 -0500

    Algorithm for scanner

--- network_scanner.py
+++ network_scanner.py
@@ -8,4 +8,20 @@ def scan(ip):
     # print(mac)
     arping(ip)
 
-scan("192.168.63.2")
+# add slash notation /24 to show all clients in range 192.168.63.1 to 192.168.63.255
+scan("192.168.63.2/24")
+
+# Network Scanner Algorithm
+# Goal -> Discover clients on a Network
+# Setups:
+# 1. Create ARP request directed to broadcast MAC asking for IP
+# 2. Send packet and receive result
+# 3. Parse the response
+# 4. Print Result
+
+
+# PROBLEM: ARP requests are logged.
+# import logging
+# logging.getLogger("scapy").setLevel(logging.CRITICAL)
+# This ensures Scapy does not print warnings or informational messages
+# Use before running scapy functions like sniff() or sent()
