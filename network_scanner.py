a63898c3f36d614edc4740c403e1b95d762c63f0
root <hackenstance@proton.me>
2026-03-21 10:17:35 -0500

    Scapy broadcast who has IP in range

--- network_scanner.py
+++ network_scanner.py
@@ -1,20 +1,20 @@
 #!usr/bin/env python
+from scapy.layers.l2 import ARP
 
-from scapy.layers.l2 import getmacbyip, arping
-# import scapy.all as scapy
-
+# Network Scanner Algorithm: Goal -> Discover clients on a Network: Setups:
+# 1. Create ARP request directed to broadcast MAC asking for IP
+# a. Use ARP to ask who has target IP
 def scan(ip):
-    # mac = getmacbyip(ip)
-    # print(mac)
-    arping(ip)
+    # create an ARP packet object
+    arp_request = ARP(pdst=ip)
+    print(arp_request.summary())
 
-# add slash notation /24 to show all clients in range 192.168.63.1 to 192.168.63.255
+
+# slash notation /24, show all clients range 192.168.63.1 to 192.168.63.255
 scan("192.168.63.2/24")
 
-# Network Scanner Algorithm
-# Goal -> Discover clients on a Network
-# Setups:
-# 1. Create ARP request directed to broadcast MAC asking for IP
+# b. Set destination MAC to broadcast MAC
+
 # 2. Send packet and receive result
 # 3. Parse the response
 # 4. Print Result
