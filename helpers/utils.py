894047fa47ce85f201ad41c43763a67326939c31
kali <hackenstance@proton.me>
2026-03-24 07:52:44 -0500

    Return IP and MAC Address from our host

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,35 +1,42 @@
 #!usr/bin/env python
 from scapy.layers.l2 import Ether, ARP, srp
 import os
-# import scapy.all as scapy
 
 def check_for_root():
-    # Require root use as scapy srp needs sudo for step 2: Send and receive a response.
     if os.geteuid() != 0:
         print("This script requires root privileges. Please run again with sudo.")
         exit(1)
 
 #(1) Create ARP request directed to broadcast MAC asking for IP
 def scan(ip):
-    # create ARP request packet
+    #(a) create ARP request packet, with MAC from IP
     arp_request = ARP(pdst=ip)
-    # print(arp_request.summary())
-
-    # Create ethernet frame with broadcast MAC
+    print(os.linesep + "ARP Request Summary: " + arp_request.summary() + os.linesep)
+    #(b) Create ethernet frame (packet) with broadcast MAC
     broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
-    # print(broadcast.summary())
-
-    # Combine the broadcast and arp_request packets
+    print("Broadcast packet summary: " + broadcast.summary() + os.linesep)
+    #(c) Combine the broadcast and arp_request packets
     broadcast_arp_request = broadcast / arp_request
-    # packet.show()
     return broadcast_arp_request
 
 #(2) send and receive response
 def send_receive_response(packet, iface, timeout, verbose):
-    # iface argument may not be needed.
-    answered, unanswered = srp(packet, iface=iface, timeout=timeout, verbose=verbose)
-    print(answered.summary())
-    return answered
+    # Option: remove unanswered_list, add [0] end srp call. Keep unanswered contents interesting.
+    answered_list, unanswered_list = srp(packet, iface=iface, timeout=timeout, verbose=verbose)
+    # print(answered_list.summary())
+    # print(unanswered_list.summary())
+
+    for sent, received in answered_list:
+        # Element first part is request sent, next the answer, separated by comma
+        # Get the IP Address
+        print(received.psrc)
+        # Get the MAC Address
+        print(received.hwsrc)
+        print("********" + os.linesep)
+
+
+
+
 
 #(3) Parse the response - answered - and extract the information we want from it.
 
--- network_scanner.py
+++ network_scanner.py
@@ -5,5 +5,5 @@ check_for_root()
 #(1) Create ARP request directed to broadcast MAC asking for IP
 broadcast_arp_response_packet = scan("192.168.1.254/24")
 #(2) send and receive response
-send_receive_response(broadcast_arp_response_packet, "eth0", 2, False)
-#(3) Parse the results
+send_receive_response(broadcast_arp_response_packet, "", 2, False)
+#(3) Parse the results, from two lists, answered and unanswered.
